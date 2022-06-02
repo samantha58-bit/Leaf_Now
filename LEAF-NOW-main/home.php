c0a235b35de094f15d646beba7e612caf4830a18
samantha58-bit <68094495+samantha58-bit@users.noreply.github.com>
2022-06-02 22:49:50 +0530

    Add files via upload

---
+++ LEAF-NOW-main/home.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Leaf Now</title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <link rel="stylesheet" href="nav.css">
+      <script src="home.js" defer></script>
+      <link rel="stylesheet" href="home.css">
+   </head>
+   <body>
+    <nav>
+        <a href="home.php" class="brand-logo">                                                     
+            <img class="logo-resize" src="" alt="">
+            <div class="brand-logo-name">LEAF NOW</div>
+        </a>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <ul class="nav-links">
+            <li><a class="link" href="home.php">Home</a></li>
+            <li><a class="link" href="donation.html">Donation</a></li>
+            <li><a class="link" href="contact_us.php">Contact Us</a></li>
+            <li><a class="link" href="signout.php">Sign </a></li>
+        </ul>
+    </nav>
+    <div class="container"></div>
+    <div class="overlay"></div>
+    <h1 style="text-align:center;">WHY Are Plants so Valuable.</h1>
+    <section class="section">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Concentration and Memory</h3>Being around plants helps people concentrate better in the home and workplace. Studies show that tasks performed while under the calming influence of nature are performed better and with greater accuracy, yielding a higher quality result. Moreover, being outside in a natural environment can improve memory performance and attention span by twenty percent. Work performed under the natural influence of ornamental plants is normally of higher quality and completed with a much higher accuracy rate than work done in environments devoid of nature.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p1.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+
+    <section class="section section-left" id="about">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Educational Programs / Special Events</h3>Botanical gardens often play host to educational programs and special events, which contribute to the cultural awareness and education of the community (children especially). This raises environmental consciousness and appreciation.Botanical gardens and zoos often create educational programs for children in order to teach them how the value of being environmentally-conscious and conserving the environment. They can also impact adults in the community as well, creating a cultural awareness of the importance of natural environments.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p20.jpg" alt="image" class="imgFluid">
+    </div> 
+    </section>
+    <section class="section" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Flowers Generate Happiness</h3> flowers around the home and office greatly improves people’s moods and reduces the likelihood of stress-related depression. Flowers and ornamental plants increase levels of positive energy and help people feel secure and relaxed. People who keep flowers in their home feel happier, less stressed, and more relaxed. As a result of the positive energy they derive from the environment, the chances of suffering from stress-related depression are decreased as well.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p3.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section section-left" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Health and Recreation</h3>Access to parks and recreational activities is positively correlated with rates of physical activity, which improves mood and contributes to overall healthiness. Health care costs are subsequently reduced.Parks and urban green spaces impact people’s health by providing them with an inexpensive (often free) and convenient recreational service. There is a positive correlation between the presence of a park in a neighborhood and the level of physical activity of the residents; people are much more likely to exercise when there is a no-cost, aesthetically pleasing area or facility for them to use.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p4.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Accelerates Healing Process</h3>The presence of plants in hospital recovery rooms and/or views of aesthetically-pleasing gardens help patients to heal faster, due to the soothing affects of ornamental horticulture.Shrubs, trees, and flowers have a practical application in hospitals: the presence of plants in patient recovery rooms greatly reduces the time necessary to heal. The soothing effects of ornamental flowers and plants are so great that simply having daily views of flowers and other ornamental plants in landscaped areas outside patient recovery room significantly speed up recovery time.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p5.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section section-left" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Improves Relationships/Compassion</h3>Research shows that people who spend extended lengths of time around plants tend to have better relationships with others. This is due to measurable increases in feelings of compassion; another effect of exposure to ornamental plants.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p6.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Learning</h3>Research shows that children who spend time around plants learn better. In addition, being around natural environments improves the ability of children with Attention Deficit Disorder to focus, concentrate, and engage more with their surrounding environment.Ornamental plants are conducive to generating a positive learning environment, reducing children’s tendency towards distraction and helping them to be better able concentrate on school work. Specifically for children with problems paying attention, adding plants to the classroom can have a dramatic positive effect on the way they learn.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p8.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section section-left" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Medicinal Properties</h3>Cultivating plants is beneficial to humankind because of the many medicinal properties of trees and foliage plants.Cultivating plants helps humanity because it provides opportunities for additional scientific studies of the possible positive medicinal values of plants. Natural herbal remedies are simple and holistic methods for treating common illnesses and maladies.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p9.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Mental Health</h3>Studies have proven that people who spend more time outside in nature have better mental health and a more positive outlook on life.People who spend more time outside in nature have a significantly more positive outlook on life than people who spend a great deal of time indoors. Communing with the natural world increases people’s feelings of vitality and energy, and consequently has a large positive effect on their overall mental health.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p10.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section section-left" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Reduce Stress</h3>Studies show that people who spend time cultivating plants have less stress in their lives. Plants soothe human beings and provide a positive way for people to channel their stress into nurturing.Participation in gardening and landscaping activities is an effective way to reduce levels of stress. Studies have shown that people who nurture plants and garden have less mental distress than others. Gardening provides people with a positive way to channel their stress and frustration into something beautiful that provides them with comfort and joy. 
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p7.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Traffic Safety / Driver Satisfaction</h3>Beautifying road ways can have the dual effect of increasing driver satisfaction with the roadside landscape and creating a natural median. Drivers are much less likely to accidentally drive over a median if there is a landscaped area between oncoming lanes of traffic.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p12.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+    <section class="section section-left" id="services">
+    <div class="paras">
+    <p class="sectionSubTag text-small"><h3>Reduce Community Crime / Community Cohesion</h3>Neighborhoods with beautiful parks tend to have less crime. This is due in part to the effect that parks have on a community; parks give people a reason to come together and become a tight-knit community. People who care about their neighborhood parks are much more likely to get politically involved when businesses threaten to downsize them. Increasing political activism in the community is another positive outcome of cultivating a love for neighborhood parks.
+    </p>
+    </div>
+    <div class="thumbnail">
+        <img src="p11.jpg" alt="image" class="imgFluid">
+    </div>
+    </section>
+   </body>
+</html>
